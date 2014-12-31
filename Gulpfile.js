c6f18b2bdf542ce515b4e8fb96c541f4032770a1
ghoullier <gregory.houllier@gmail.com>
2014-12-31 17:10:40 +0100

    Add minimify html

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,12 +2,18 @@
 
 var gulp    = require('gulp');
 var ghPages = require('gulp-gh-pages');
+var htmlmin = require('gulp-htmlmin');
 
 var pkg     = require('./package');
 
 // Deploy task
 gulp.task('deploy', function deploy() {
-  return gulp.src('./src/**/*.*')
+  return gulp.src('./src/index.html')
+    // Minimify html
+    .pipe(htmlmin({
+      collapseWhitespace: true
+    }))
+    // Publish on GitHub Pages
     .pipe(ghPages({
       remoteUrl : pkg.repository.url,
       branch : 'gh-pages',
