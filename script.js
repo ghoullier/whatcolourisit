a3520284154ace1988c4b75ca656d9efc334a45d
ghoullier <gregory.houllier@gmail.com>
2015-01-01 14:50:08 +0100

    Update 2015-01-01T13:50:07.625Z

--- script.js
+++
@@ -1,1 +0,0 @@
-(function main() { var time = document.querySelector('.Color__Time'); var code = document.querySelector('.Color__Code'); var style = document.body.style; (function tick() { var now = moment(); time.innerText = now.format('HH : mm : ss'); style.backgroundColor = code.innerText = now.format('#HHmmss'); setTimeout(tick, 1000); }(style.width)); }());
